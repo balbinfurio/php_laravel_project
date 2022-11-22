77b55b3873dd2fb5f51fab0d31f672144e142dfb
balbinfurio <xxbalbin@gmail.com>
2022-11-22 11:44:17 -0500

    controladores y request

--- curso_laravel/routes/web.php
+++ curso_laravel/routes/web.php
@@ -1,35 +1,29 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome'); //welcome es el nombre del archivo alojado en resources/views 
-});
 
-Route::get('/formato_json', function() {
-    return [
-        'saludo' => 'Hola',
-        'nombre' => 'Furio'
-    ];
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/carpeta_anidada/archivo_anid', function() {
-    return view('carpeta_anidada.archivo_anid'); // ejemplo con carpeta y archivo anidados
-});
+Route::get('/dashboard', [DashboardController::class, 'index']); 
 
-Route::get('/test', function() {
-    return view('test', [
-        'tittle' => 'Curso Laravel Platzi!!'
-    ]);
-});
+
+// Route::get('/formato_json', function() {
+//     return [
+//         'saludo' => 'Hola',
+//         'nombre' => 'Furio'
+//     ];
+// });
+
+// Route::get('/carpeta_anidada/archivo_anid', function() {
+//     return view('carpeta_anidada.archivo_anid'); // ejemplo con carpeta y archivo anidados
+// });
+
+// Route::get('/test', function() {
+//     return view('test', [
+        
+//     ]);
+// });
